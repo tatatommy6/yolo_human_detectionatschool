ee03294004abb6b4b29799f86c5caa0e4d82badd
김민결 <139607260+tatatommy6@users.noreply.github.com>
2025-06-10 01:53:57 +0900

    ?

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ model = YOLO("yolo11n.pt")
 #커스텀 데이터 학습할때 쓰는 코드
 model.train(
     data = "dataset.yaml",  
-    epochs = 10,  
+    epochs = 100,  
     imgsz =  640,  
     device = "mps",  
     name = "crowd_detector"
